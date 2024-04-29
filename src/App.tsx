ccc18f91bfb3ce172cd3189b546624f189d66f3c
Kirk Steineck <stein935@gmail.com>
2024-04-29 15:30:01 -0500

    fonts, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu-mono";
+import "@fontsource/merriweather";
+import "@fontsource/alfa-slab-one";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Kirk Steineck</h1>
+      <div className="prose prose-stone prose-md container mx-4 space-y-4 font-serif antialiased">
+        <div className="pt-4">
+          <h1 className="font-display mb-0">Kirk Steineck</h1>
+          <p className="mt-0">stein935@gmail.com</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
